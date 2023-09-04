b3a56f5502990d8e0318fb34b2e964642456c100
bazayd <brandonzarriaga@gmail.com>
2023-09-03 18:51:15 -0700

    Added getters and setters for user information such as name, password, capacity, etc.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,43 @@
 package org.example;
+import com.github.curiousoddman.rgxgen.RgxGen;
+import com.github.curiousoddman.rgxgen.iterators.StringIterator;
+import java.math.BigInteger;
+import java.util.Optional;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        /**
+         * Ask user for their first and last name, department they were hired for, mail capacity, and
+         * their desired password. Generated password if none is selected.
+         */
+        try {
+            System.out.println("Enter your first name: ");
+            String firstName = scanner.next();
+
+            System.out.println("Enter last name: ");
+            String lastName = scanner.next();
+
+            System.out.println("Department: ");
+            String department = scanner.next();
+
+            System.out.println("Mail capacity: ");
+            int capacity = scanner.nextInt();
+
+            System.out.println("Enter Password (Leave blank for random password):");
+            String password = scanner.next();
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+//        User brandon = new User();
 
 
     }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,76 @@
 package org.example;
 
+import com.github.curiousoddman.rgxgen.RgxGen;
+import com.github.curiousoddman.rgxgen.iterators.StringIterator;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+/**
+ * User class creating user email/account.
+ * Ex: firstname.lastname@department.company.com
+ */
 public class User {
+
+    String firstName;
+    String lastName;
+    String department;
+    int capacity;
+    String password;
+
+
+    public User(String firstName, String lastName, String department, String password, int capacity) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.password = password;
+        this.capacity = capacity;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+
+    public int getCapacity() {
+
+        return capacity;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+
+    /**
+     * Generate randomvpassword for user if a password is not manually created.
+     * @return password
+     */
+    public String generatePassword() {
+
+        RgxGen rgxGen = RgxGen.parse("[^A-Z]*[a-z]?[0-9]");
+        String password = rgxGen.generate();
+        Optional<BigInteger> estimation = rgxGen.getUniqueEstimation();      // The estimation (not accurate, see Limitations) how much unique values can be generated with that pattern.
+        StringIterator uniqueStrings = rgxGen.iterateUnique();               // Iterate over unique values (not accurate, see Limitations)
+        String notMatching = rgxGen.generateNotMatching();                   // Generate not matching string
+
+        return password;
+    }
+
+    public String constructEmail(String firstName, String lastName, String department) {
+        String email = firstName + "." + lastName + "@" + department + ".company.com";
+
+        return email;
+    }
+
+
+
 }
